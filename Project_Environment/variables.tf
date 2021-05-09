33a81f7ed96d5cfb5346ecda2ba8d32eea8faeb0
Giuseppe Superbo <giuseppe.superbo97@gmail.com>
2021-05-09 17:00:37 +0200

    Add missing variable declaration

--- Project_Environment/variables.tf
+++ Project_Environment/variables.tf
@@ -6,6 +6,18 @@ variable "project_name" {
 }
 
 variable "short_project_name" {
-    type = string
-    default = "lod"
+  type = string
+  default = "lod"
+}
+
+variable "password" {
+  type = string
+}
+
+variable "user_id_list" {
+  type = list(string)
+}
+
+variable "member_role_id" {
+  type = string
 }
