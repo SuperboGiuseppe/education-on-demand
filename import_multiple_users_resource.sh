0a8fb9896972979db060aec163ecc5d764bf03cd
Giuseppe Superbo <giuseppe.superbo97@gmail.com>
2021-05-09 16:31:10 +0200

    Edit project terraform template

--- import_multiple_users_resource.sh
+++ import_multiple_users_resource.sh
@@ -1,7 +1,16 @@
-users_test=( "$@" )
-declare -a output_arr=()
-for user in "${users[@]}"
+input_users=( "$@" )
+declare -a id_users=()
+for user in "${input_users[@]}"
         do
                 output_json_user_id=`openstack user show -f json $user`
-                output_arr+=(`echo $output_json_user_id | jq -r '.id'`)
+                id_users+=(`echo $output_json_user_id | jq -r '.id'`)
         done
+output_list="["
+
+for id in "${id_users[@]}"
+        do
+                output_list="$output_list $id,"
+        done
+
+output_list="$output_list]"
+TF_VAR_user_id_list=$output_list
--- project_environment.tf
+++ project_environment.tf
@@ -12,20 +12,15 @@ resource "openstack_identity_user_v3" "eval_user" {
     password = var.password
 }
 
-resource "openstack_identity_role_assignment_v3" "project_membership" {
+resource "openstack_identity_role_assignment_v3" "project_membership_fixed" {
   user_id    = "${openstack_identity_user_v3.eval_user.id}"
   project_id = "${openstack_identity_project_v3.term_project.id}"
   role_id    = var.member_role_id
 }
 
-resource "openstack_identity_role_assignment_v3" "project_membership" {
-  user_id    = "${openstack_identity_user_v3.eval_user.id}"
-  project_id = "${openstack_identity_project_v3.term_project.id}"
-  role_id    = var.member_role_id
-}
-
-resource "openstack_identity_role_assignment_v3" "project_membership" {
-  user_id    = "${openstack_identity_user_v3.eval_user.id}"
+resource "openstack_identity_role_assignment_v3" "project_membership_dynamic" {
+  count=length(var.user_id_list)
+  user_id = var.user_id_list[count.index]
   project_id = "${openstack_identity_project_v3.term_project.id}"
   role_id    = var.member_role_id
 }
